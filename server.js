d8430e5c3453b57fb0c69569a83d142cbb842f15
Grant Stampfli <mail@gstampfli.com>
2014-06-16 16:29:30 -0700

    fixed typos

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
     var http = require('http');
     var path = require ('path');
     var fs = require ('fs');
-    http.createServer(function (eq, res) {
+    http.createServer(function (req, res) {
     	var file = path.join(__dirname, 'sample.html');
     	console.log('%s %s', req.connection.remoteAddress, req.url);
     	var fileStream = fs.createReadStream(file);
     		fileStream.pipe(res);
 
-    }).losten(3030);
+    }).listen(3030);
