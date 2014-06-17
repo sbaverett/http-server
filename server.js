7fc43b1d087355da5b002f356c6ba53dc9f7caa1
Sam Averett <averettsam@comcast.net>
2014-06-17 09:09:19 -0700

    now loads all requested files

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var path = require ('path');
 var fs = require ('fs');
 
 http.createServer(function (request, response) {
-	var filePath = path.join(__dirname, 'public', 'index.html');
 	console.log('%s %s', request.connection.remoteAddress, request.url);
+
+	var filePath = path.join(__dirname, 'public', request.url);
 	var fileStream = fs.createReadStream(filePath);
 	fileStream.pipe(response);
 
